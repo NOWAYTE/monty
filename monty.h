eade5a84fd4ae6d0175408e8f85dadb5c835e745
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-24 22:31:54 +0300

    Fix compilation errors

--- monty.h
+++ monty.h
@@ -56,3 +56,5 @@ void monty_push(stack_t **stack, unsigned int line_number);
 void monty_pall(stack_t **stack, unsigned int line_number);
 void monty_pint(stack_t **stack, unsigned int line_number);
 void monty_pop(stack_t **stack, unsigned int line_number);
+
+#endif
