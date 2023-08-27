79eea2c1cc4bdf7f8cd4863ee4533c2d4d8e4a6c
Daniel kamau nganga <danielkamaunganga@gmail.com>
2023-08-27 23:14:24 +0300

    Fix code compilation errors

--- _func3.c
+++ _func3.c
@@ -91,4 +91,38 @@ void _swap(stack_t **head, unsigned int counter)
 	h->n = h->next->n;
 	h->next->n = ax;
 }
+/**
+ * a_queue - add node to the tail stack
+ * @n: new_value
+ * @head: head of the stack
+ * Return: no return
+*/
+void a_queue(stack_t **head, int n)
+{
+	stack_t *n_node, *ax;
+
+	ax = *head;
+	n_node = malloc(sizeof(stack_t));
+	if (n_node == NULL)
+	{
+		printf("Error\n");
+	}
+	n_node->n = n;
+	n_node->next = NULL;
+	if (ax)
+	{
+		while (ax->next)
+			ax = ax->next;
+	}
+	if (!ax)
+	{
+		*head = n_node;
+		n_node->prev = NULL;
+	}
+	else
+	{
+		ax->next = n_node;
+		n_node->prev = ax;
+	}
+}
 
